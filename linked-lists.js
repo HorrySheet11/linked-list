972c5dea3c1f3bce70ac81e25882ed4a04e25314
HorrySheet11 <marinnathan11@gmail.com>
2025-11-20 19:04:21 +0800

    completed all functions in linked-lists.js
    working linked list

--- linked-lists.js
+++ linked-lists.js
@@ -1,64 +1,114 @@
-const LinkedList = () => {
-    let listSize = 0;
-    let head = null
-    const append = (value) => {
-        let newNode = new Node(value);
-        if(!head) {
-            head = newNode;
-        } else {
-            let current = head;
-            while(current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-        listSize++;
-    }
-    const prepend = (value) => {
-        let newNode = new Node(value);
-        newNode.next = head;
-        head = newNode;
-        listSize++;
-    }
-    const size = () => {
-        return listSize;
-    }
-    const checkHead = () => {
-        if(!head) return null;
-        
-        return head.value;
-    }
-    const checkTail = () => {    
-        return list[-1];
-    }
-    const at = (index) => {
-        return list[index];
-    }
-    const pop = () => {
-        list.pop();
-    }
-    const contains = (value) => {   
-        if(!value) return false; 
-        return list.includes(value);
-    }
-    const find = (value) => {    
-        return list.indexOf(value);
-    }
-    const convertToString = () => {    
-        return list.toString();
-    }
-    // const insertAt = (value, index) => {
+class LinkedList {
+	constructor() {
+		this.listSize = 0;
+		this.head = null;
+	}
 
-    // }
-    // const removeAt = (index) => {
+	append(value) {
+		const newNode = new Node(value);
+		if (!this.head) {
+			this.head = newNode;
+		} else {
+			let current = this.head;
+			while (current.next) {
+				current = current.next;
+			}
+			current.next = newNode;
+		}
+		this.listSize++;
+	}
 
-    // }
-    return 
+	prepend(value) {
+		const newNode = new Node(value);
+		newNode.next = this.head;
+		this.head = newNode;
+		this.listSize++;
+	}
+
+	size() {
+		console.log(`LinkList size: ${this.listSize}`);
+	}
+
+	checkHead() {
+		console.log(`Head value: ${this.head.value}`);
+	}
+
+	checkTail() {
+		if (!this.head) return null;
+		let current = this.head;
+		while (current.next) {
+			current = current.next;
+		}
+		console.log(`Tail value: ${current.value}`);
+	}
+
+	at(index) {
+		let current = this.head;
+		for (let i = 0; i < index; i++) {
+			current = current.next;
+		}
+		console.log(`Value at index ${index}: ${current.value}`) ;
+	}
+
+	pop() {
+		let current = this.head;
+		while (current.next.next) {
+			current = current.next;
+		}
+		current.next = null;
+		this.listSize--;
+	}
+
+	contains(value) {
+		while (this.head) {
+			if (this.head.value === value) {
+                console.log(`${value} is in the list`);
+                return true};
+			this.head = this.head.next;
+		}
+        console.log(`${value} is not in the list`);
+        return false;
+	}
+
+	find(value) {
+		let current = this.head;
+		let index = 0;
+		while (current) {
+			if (current.value === value) return index;
+			current = current.next;
+			index++;
+		}
+        console.log(`Index of ${value}: ${index}`);
+	}
+
+	convertToString() {
+		let current = this.head;
+		let result = "";
+		while (current) {
+			result += ` ( ${current.value} ) -> `;
+			current = current.next;
+		}
+		console.log(`${result} null`);
+	}
 }
 
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
 }
+
+const list = new LinkedList();
+list.append(10);
+list.append(20);
+list.prepend(30);
+list.convertToString();
+list.size();
+list.find(20);
+list.at(1);
+list.checkHead();
+list.checkTail();
+list.pop();
+list.convertToString();
+list.contains(20);
